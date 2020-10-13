c0ca3d3391659557d83ee19532a593084a1ec87e
LQLliqilin <72777379+LQLliqilin@users.noreply.github.com>
2020-10-13 11:17:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lql/util/DateUtil.java
@@ -0,0 +1,11 @@
+package com.example.lql.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class DateUtil {
+
+    public static  String getNowTime(){
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+        return df.format(new Date());
+    }
+}
